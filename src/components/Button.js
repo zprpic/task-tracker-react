c565e72eaed78499bacf2929503b39995af9b7d6
zprpic <prpiczvonimirprpiczvonimir@gmail.com>
2021-09-26 19:54:25 +0200

    Prepared an edit icon...

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ReactComponent as EditIcon } from "../assets/edit.svg";
 
 export const Button = (props) => {
   const { id } = props;
   return (
     <button>
-      <Link to={`/tasks/${id}`}>Edit...</Link>
+      <Link to={`/tasks/${id}`}>
+        <EditIcon />
+      </Link>
     </button>
   );
 };
